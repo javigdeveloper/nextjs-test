21592ba8da0f1e5fbd76d25a2c7299a25eea8733
Javier García <javigdeveloper@gmail.com>
2021-02-18 22:23:06 -0500

    add style adjustments

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div>Copyright 2021</div>;
+  return <footer>Copyright 2021 People list</footer>;
 };
 
 export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,14 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>My Next JS app</h1>
+      <h1 className={styles.title}>My Next JS app</h1>
+      <p className={styles.text}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo cumque
+        quas asperiores ullam minima in, error enim iusto, sequi reiciendis,
+        perspiciatis exercitationem dolorem nobis distinctio similique fugit
+        natus accusamus? Beatae, ipsa ratione! Officiis maiores illo tempora
+        quam? Mollitia, repudiandae eveniet?
+      </p>
     </div>
   );
 }
